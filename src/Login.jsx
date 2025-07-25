4c63c13ee65b91ec4bee0858ff1474b4ae1fa611
RudraPrasad001 <rudraprasad.email@gmail.com>
2025-07-25 20:59:55 +0530

    Merge Fix

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import Cookies from "js-cookie"; 
-import axios from "axios";
 import './login.css';
 
 const Login = () => {
